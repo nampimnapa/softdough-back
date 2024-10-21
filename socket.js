a34d23165aade46e98f758acae2ba6b5eb73acd2
OSP101 <supphitan.pk@gmail.com>
2024-10-22 04:46:01 +0700

    config socket for docker

--- socket.js
+++ socket.js
@@ -4,33 +4,33 @@ require('dotenv').config();
 const frontUrl = process.env.FRONT;
 
 const setupSocket = (server) => {
-    const io = socketIo(server, {
-        cors: {
-          origin: process.env.FRONT,
-          methods: ["GET", "POST"],
-          allowedHeaders: ["Content-Type", "Authorization"],
-          credentials: true,
-          secure: true,
-        }
-      });
+  const io = socketIo(server, {
+    cors: {
+      origin: process.env.FRONT,
+      methods: ["GET", "POST"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+      credentials: true,
+    },
+    transports: ['websocket']
+  });
 
-    io.on('connection', (socket) => {
-        console.log('User connected:', socket.id);
-        socket.on('registerUser', (userId) => {
-            socket.join(userId); // เก็บ Socket ตาม userId
-            console.log('User registered:', userId);
-        });
-
-        socket.on('disconnect', () => {
-            console.log('User disconnected:', socket.id);
-        });
+  io.on('connection', (socket) => {
+    console.log('User connected:', socket.id);
+    socket.on('registerUser', (userId) => {
+      socket.join(userId); // เก็บ Socket ตาม userId
+      console.log('User registered:', userId);
     });
 
-    io.on('connect_error', (error) => {
-        console.error('Connection error:', error);
-      });
+    socket.on('disconnect', () => {
+      console.log('User disconnected:', socket.id);
+    });
+  });
 
-    return io; // คืนค่า io เพื่อให้ใช้งานได้ในที่อื่น
+  io.on('connect_error', (error) => {
+    console.error('Connection error:', error);
+  });
+
+  return io; // คืนค่า io เพื่อให้ใช้งานได้ในที่อื่น
 };
 
 module.exports = setupSocket;

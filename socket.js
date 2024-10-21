0ccb4389a53dcd5d7af1993418add35ac29d3d61
OSP101 <supphitan.pk@gmail.com>
2024-10-22 06:12:52 +0700

    config CORS

--- socket.js
+++ socket.js
@@ -6,7 +6,7 @@ const frontUrl = process.env.FRONT;
 const setupSocket = (server) => {
   const io = socketIo(server, {
     cors: {
-      origin: '*',
+      origin: [frontUrl, 'http://localhost:3000'],
       methods: ["GET", "POST"],
       allowedHeaders: ["Content-Type", "Authorization"],
       credentials: true,
